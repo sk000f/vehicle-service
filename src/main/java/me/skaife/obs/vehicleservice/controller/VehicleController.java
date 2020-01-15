316bdb32238201ec6252d5448cf10d9a77736834
Mike Skaife <mskaife@jaguarlandrover.com>
2020-01-15 08:55:39 +0000

    Logback structured logging config and example

--- src/main/java/me/skaife/obs/vehicleservice/controller/VehicleController.java
+++ src/main/java/me/skaife/obs/vehicleservice/controller/VehicleController.java
@@ -1,5 +1,8 @@
 package me.skaife.obs.vehicleservice.controller;
 
+import org.jboss.logging.MDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +12,23 @@ import me.skaife.obs.vehicleservice.model.Vehicle;
 import me.skaife.obs.vehicleservice.model.VehicleRequest;
 import me.skaife.obs.vehicleservice.model.VehicleResponse;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 
 @RestController
 public class VehicleController {
 
+  private static final Logger logger = LoggerFactory.getLogger(VehicleController.class);
+
   @GetMapping(path="/vehicles")
   public Vehicle getVehicles() {
+
+      MDC.put("userId", "MSKAIFE");
+      logger.info("GET Vehicles endpoint called");
+
+      MDC.put("errorMessage", "Something has gone terribly wrong");
+      logger.error("Error in GET Vehicles endpoint");
+
       Vehicle vehicle = new Vehicle();
       vehicle.setId(1L);
       vehicle.setManufacturer("Jaguar");
@@ -25,6 +39,11 @@ public class VehicleController {
 
   @PostMapping(path="/vehicles")
   public VehicleResponse addVehicle(@RequestBody VehicleRequest inputPayload) {
+
+    logger.info("GET Vehicles endpoint called", kv("username", "TFERGUS2"));
+
+    logger.error("Error in GET Vehicles endpoint", kv("errorMessage", "Something has gone even more badly wrong"));
+
     VehicleResponse response = new VehicleResponse();
     response.setId(inputPayload.getId());
     response.setManufacturer(inputPayload.getManufacturer());
