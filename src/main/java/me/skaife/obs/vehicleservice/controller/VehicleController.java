37cff93af3c799da6fd6dd37524a1e6d0f46ec91
Mike Skaife <mskaife@jaguarlandrover.com>
2020-01-16 11:16:29 +0000

    Dockerfile and image build in pom

--- src/main/java/me/skaife/obs/vehicleservice/controller/VehicleController.java
+++ src/main/java/me/skaife/obs/vehicleservice/controller/VehicleController.java
@@ -1,8 +1,8 @@
 package me.skaife.obs.vehicleservice.controller;
 
 import org.jboss.logging.MDC;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
